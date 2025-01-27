454cc319d8ab4e6306f5920fc495be1a94c0c91b
a <a@gmail.com>
2025-01-27 10:17:22 +0200

    Log In & Sign Up are working

--- TetrisApp/SignUpPage.xaml.cs
+++ TetrisApp/SignUpPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Model;
+using MyService;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +22,18 @@ namespace TetrisApp
     /// </summary>
     public partial class SignUpPage : Page
     {
+        private Language ChoosenLanguage;
         public SignUpPage()
         {
             InitializeComponent();
+
+            SetContentForLangugesComboBox();
+        }
+
+        private async void SetContentForLangugesComboBox()
+        {
+            Apiservice api = new();
+            LanguagesComboBox.ItemsSource = await api.GetAllLanguages();
         }
 
         private void GoBack(object sender, MouseButtonEventArgs e)
@@ -35,14 +46,39 @@ namespace TetrisApp
 
         }
 
-        private void SignUp(object sender, RoutedEventArgs e)
-        {
+        private async void SignUp(object sender, RoutedEventArgs e)
+        {            
+            Apiservice APIservice = new();
+            
+            User GivenUser = new User() { Password = PasswordBox.Text, UserName = UsernameBox.Text, language =this.ChoosenLanguage, ProfilePicture=""};
 
+            List<User> users = await APIservice.GetAllUsers();
+
+            User potentialUser = users.Find(x => x.UserName == GivenUser.UserName);
+
+            if (potentialUser != null)
+            {
+                this.ErrorSigningUpText.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                this.ErrorSigningUpText.Visibility = Visibility.Hidden;
+                APIservice.InsertUser(GivenUser);
+                MessageBox.Show("Account added");
+            }
         }
 
         private void ChooseImageBtn(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private async void LanguageChosen(object sender, SelectionChangedEventArgs e)
+        {
+            Apiservice api = new();
+            List<Language> languages = await api.GetAllLanguages();
+
+            this.ChoosenLanguage = languages[this.LanguagesComboBox.SelectedIndex];
         }
     }
 }
